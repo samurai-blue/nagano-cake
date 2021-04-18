7c05c700268a1f182a257b9723ed0bdff8aa96a9
ryuseitaked <ryutake0306@gmail.com>
2021-04-18 00:38:13 +0000

    ジャンル登録機能雛形

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # ジャンルのURLを「admin/genres」にする
+  scope :admin do
+    resources :genres
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :genres
 end
+
