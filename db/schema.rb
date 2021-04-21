b69bd3d8ce8965d74ff4c0cc601b8f787ac65e6e
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-04-21 08:46:53 +0000

    view整理

--- db/schema.rb
+++ db/schema.rb
@@ -44,28 +44,6 @@ ActiveRecord::Schema.define(version: 2021_04_20_044455) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "postal_code"
-    t.text "address"
-    t.string "name"
-    t.integer "shipping_cost"
-    t.integer "total_payment"
-    t.integer "payment_method"
-    t.integer "order_status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "shippings", force: :cascade do |t|
-    t.string "postal_code"
-    t.text "address"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-
   create_table "items", force: :cascade do |t|
     t.text "name"
     t.string "image_id"
@@ -77,13 +55,6 @@ ActiveRecord::Schema.define(version: 2021_04_20_044455) do
     t.boolean "is_saled"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id"
     t.string "postal_code"
@@ -93,13 +64,11 @@ ActiveRecord::Schema.define(version: 2021_04_20_044455) do
     t.integer "total_payment"
     t.integer "payment_method"
     t.integer "order_status"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "shippings", force: :cascade do |t|
-    t.integer "customer_id"
     t.string "postal_code"
     t.text "address"
     t.string "name"
@@ -107,5 +76,16 @@ ActiveRecord::Schema.define(version: 2021_04_20_044455) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
