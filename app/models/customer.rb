ac384ca2d1dbf0d2adab6c08ee1ecdbc4f85df7a
vullbyte <oto10tis@outlook.jp>
2021-04-21 06:02:14 +0000

    [Add]管理人ログイン機能、会員一覧詳細編集

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,5 +8,12 @@ class Customer < ApplicationRecord
               :postal_code, :address, :telephone_number,
               presence: true
 
-
+    # 会員フルネーム
+  def full_name
+    self.last_name + " " + self.first_name
+  end
+  def kana_full_name
+    self.last_name_kana + " " + self.first_name_kana
+  end  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :admins, controllers: {sessions: 'admins/sessions'}
+  namespace :admin do
+    resources :customers,only: [:index,:show,:edit,:update]
+  end
   devise_for :customers
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 
