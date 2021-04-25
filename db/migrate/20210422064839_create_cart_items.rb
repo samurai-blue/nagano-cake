88c586de58e0b45d57547cd08a6e4e933b34b2ae
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-04-25 04:51:01 +0000

    [add]img

--- db/migrate/20210422064839_create_cart_items.rb
+++ db/migrate/20210422064839_create_cart_items.rb
@@ -2,7 +2,7 @@ class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_items do |t|
       t.integer :quantity
-      t.integer :items_id
+      t.integer :item_id
       t.integer :customer_id
 
       t.timestamps
