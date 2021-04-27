8f7497e9ab69ba39c9066d3fad901d0fa0e1e286
ryuseitaked <ryutake0306@gmail.com>
2021-04-27 09:18:26 +0000

    m

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,7 @@ class Order < ApplicationRecord
   validates :postal_code, presence: true
   validates :send_to_address, presence: true
 
-  #enum_支払い方法
+  enum_支払い方法
   #enum payment_method: {"クレジットカード": 0,"銀行振込": 1}
   #注文ステータス
 	#enum order_status: {"入金待ち": 0,"入金確認": 1,"製作中": 2,"発送準備中": 3, "発送済み": 4}
